b72b27122a846dca55f1ffddd046a135ac9335e7
rantao <rantao@163.com>
2019-01-04 17:31:49 +0800

    1.4

--- src/main/java/tech/wetech/admin/modules/system/web/FixedAreaController.java
+++ src/main/java/tech/wetech/admin/modules/system/web/FixedAreaController.java
@@ -3,7 +3,6 @@ package tech.wetech.admin.modules.system.web;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,29 +19,40 @@ import javax.validation.Valid;
  * @author rt
  */
 @Controller
-@RequestMapping("/fixedArea/")
+@RequestMapping("/fixedArea")
 public class FixedAreaController {
     @Autowired
     private FixedAreaService fixedAreaService;
 
     @GetMapping
     @RequiresPermissions("fixedarea:view")
-    public String page(Model model) {
+    public String page() {
         return "system/fixedarea/fixedarealist";
     }
 
     @ResponseBody
-    @RequestMapping("list")
-    @RequiresPermissions("fixedarea:view")
+    @RequestMapping("/list")
+    //@RequiresPermissions("fixedarea:view")
     public PageResultSet<FixedAreaDto> list(FixedAreaQuery fixedAreaQuery) {
+        System.out.print("list");
         return fixedAreaService.findByPage(fixedAreaQuery);
     }
 
     @ResponseBody
     @RequiresPermissions("fixedarea:create")
-    @RequestMapping("addFix")
+    @RequestMapping("/create")
     public Result insert(@Valid FixedArea fixedArea){
         fixedAreaService.insertOne(fixedArea);
+        return  Result.success();
+    }
+
+    @ResponseBody
+    @RequestMapping("/checkNo")
+    public Result checkNo(@Valid FixedArea fixedArea){
+        fixedArea = fixedAreaService.selectOne(fixedArea);
+        if(fixedArea != null){
+            return  Result.failure("400","已存在");
+        }
         return  Result.success();
     }
 }
