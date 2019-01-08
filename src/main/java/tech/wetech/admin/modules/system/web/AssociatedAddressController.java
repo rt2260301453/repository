2e3f60dedd16077043fc658e8064238d8e2ac35a
zhuchunchen <zhuchunchen@163.com>
2019-01-08 15:24:01 +0800

    2ddd

--- src/main/java/tech/wetech/admin/modules/system/web/AssociatedAddressController.java
+++ src/main/java/tech/wetech/admin/modules/system/web/AssociatedAddressController.java
@@ -23,69 +23,69 @@ import java.util.Arrays;
 /**
  * @author rt
  */
-@Controller
-@RequestMapping("/associatedAddress")
+//@Controller
+//@RequestMapping("/associatedAddress")
 public class AssociatedAddressController {
-    @Autowired
-    private FixedAreaService fixedAreaService;
-
-    @GetMapping
-    @RequiresPermissions("fixedarea:view")
-    public String page() {
-        setCommonData(model);
-        return "system/fixedarea/associatedaddresslist";
-    }
-
-    @ResponseBody
-    @RequestMapping("/list")
-    @RequiresPermissions("fixedarea:view")
-    public PageResultSet<FixedAreaDto> list(FixedAreaQuery fixedAreaQuery) {
-        System.out.println("********************************************************");
-        System.out.println("fixedName="+fixedAreaQuery.getFixedareaname());
-        System.out.println("fixedManager="+fixedAreaQuery.getFixedareamanager());
-        return fixedAreaService.findByPage(fixedAreaQuery);
-    }
-
-    @ResponseBody
-    @RequiresPermissions("fixedarea:create")
-    @RequestMapping("/create")
-    public Result insert(@Valid FixedArea fixedArea){
-        fixedAreaService.insertOne(fixedArea);
-        return  Result.success();
-    }
-
-    @ResponseBody
-    @RequiresPermissions("fixedarea:update")
-    @RequestMapping("/update")
-    @SystemLog("定区管理更新定区")
-    public Result update(@Valid FixedArea fixedArea){
-        fixedAreaService.updateOne(fixedArea);
-        return  Result.success();
-    }
-
-    @ResponseBody
-    @RequiresPermissions("fixedarea:delete")
-    @RequestMapping("/delete")
-    @SystemLog("定区管理删除定区")
-    public Result delete(@RequestParam("id") Long[] ids){
-        Arrays.asList(ids).forEach(id -> fixedAreaService.deleteOne(id));
-        return Result.success();
-    }
-
-    @RequestMapping("/checkNo")
-    public void checkNo(@Valid FixedArea fixedArea, HttpServletResponse response){
-        int count = fixedAreaService.selectOne(fixedArea);
-        System.out.println("fixedNo="+fixedArea.getFixedareano());
-        if(count == 1){
-            response.setStatus(400);
-        }else {
-            response.setStatus(200);
-        }
-    }
-
-    private void setCommonData(Model model) {
-        model.addAttribute("organizationList", organizationService.findAll());
-        model.addAttribute("roleList", roleService.findAll());
-        model.addAttribute("groupList",groupService.findAll());
-    }
+//    @Autowired
+//    private FixedAreaService fixedAreaService;
+//
+//    @GetMapping
+//    @RequiresPermissions("fixedarea:view")
+//    public String page() {
+//        setCommonData(model);
+//        return "system/fixedarea/associatedaddresslist";
+//    }
+//
+//    @ResponseBody
+//    @RequestMapping("/list")
+//    @RequiresPermissions("fixedarea:view")
+//    public PageResultSet<FixedAreaDto> list(FixedAreaQuery fixedAreaQuery) {
+//        System.out.println("********************************************************");
+//        System.out.println("fixedName="+fixedAreaQuery.getFixedareaname());
+//        System.out.println("fixedManager="+fixedAreaQuery.getFixedareamanager());
+//        return fixedAreaService.findByPage(fixedAreaQuery);
+//    }
+//
+//    @ResponseBody
+//    @RequiresPermissions("fixedarea:create")
+//    @RequestMapping("/create")
+//    public Result insert(@Valid FixedArea fixedArea){
+//        fixedAreaService.insertOne(fixedArea);
+//        return  Result.success();
+//    }
+//
+//    @ResponseBody
+//    @RequiresPermissions("fixedarea:update")
+//    @RequestMapping("/update")
+//    @SystemLog("定区管理更新定区")
+//    public Result update(@Valid FixedArea fixedArea){
+//        fixedAreaService.updateOne(fixedArea);
+//        return  Result.success();
+//    }
+//
+//    @ResponseBody
+//    @RequiresPermissions("fixedarea:delete")
+//    @RequestMapping("/delete")
+//    @SystemLog("定区管理删除定区")
+//    public Result delete(@RequestParam("id") Long[] ids){
+//        Arrays.asList(ids).forEach(id -> fixedAreaService.deleteOne(id));
+//        return Result.success();
+//    }
+//
+//    @RequestMapping("/checkNo")
+//    public void checkNo(@Valid FixedArea fixedArea, HttpServletResponse response){
+//        int count = fixedAreaService.selectOne(fixedArea);
+//        System.out.println("fixedNo="+fixedArea.getFixedareano());
+//        if(count == 1){
+//            response.setStatus(400);
+//        }else {
+//            response.setStatus(200);
+//        }
+//    }
+//
+//    private void setCommonData(Model model) {
+//        model.addAttribute("organizationList", organizationService.findAll());
+//        model.addAttribute("roleList", roleService.findAll());
+//        model.addAttribute("groupList",groupService.findAll());
+//    }
 }
---
+++ src/main/java/tech/wetech/admin/modules/system/web/ReplayController.java
@@ -0,0 +1,10 @@
+package tech.wetech.admin.modules.system.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/staff")
+public class ReplayController {
+    
+}
