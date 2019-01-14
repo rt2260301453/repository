06d5031549c4644c5325fa92d371d6271cc5c966
yanchen <yanchen@163.com>
2019-01-14 09:15:17 +0800

    1.14

--- src/main/java/tech/wetech/admin/core/utils/ResultCodeEnum.java
+++ src/main/java/tech/wetech/admin/core/utils/ResultCodeEnum.java
@@ -5,7 +5,7 @@ package tech.wetech.admin.core.utils;
  */
 public enum ResultCodeEnum {
 
-<<<<<<< HEAD
+
     OK("200", "´¦Àí³É¹¦"),
     BAD_REQUEST("400", "ÇëÇó²ÎÊýÓÐÎó"),
     UNAUTHORIZED("401", "Î´ÊÚÈ¨"),
@@ -17,19 +17,9 @@ public enum ResultCodeEnum {
     NOT_IMPLEMENTED("501", "ÒµÎñÒì³£"),
     FAILED_BASICNAME_ALREADY_EXIST("444", "¸Ãµµ°¸Ãû³ÆÒÑ´æÔÚ"),
     FAILED_BASICNO_ALREADY_EXIST("445", "¸Ãµµ°¸±àºÅÒÑ´æÔÚ"),
-    FAILED_CLASSIFYING_ALREADY_EXIST("446","ÊÇ·ñ·Ö¼¶Ñ¡Ôñ¡°ÊÇ¡±ºó,²»ÄÜÐÞ¸ÄÎª¡°·ñ¡±£¡£¡");
-=======
-    OK("200", "å¤„ç†æˆåŠŸ"),
-    BAD_REQUEST("400", "è¯·æ±‚å‚æ•°æœ‰è¯¯"),
-    UNAUTHORIZED("401", "æœªæŽˆæƒ"),
-    PARAMS_MISS("483", "ç¼ºå°‘æŽ¥å£ä¸­å¿…å¡«å‚æ•°"),
-    PARAM_ERROR("484", "å‚æ•°éžæ³•"),
-    FAILED_DEL_OWN("485", "ä¸èƒ½åˆ é™¤è‡ªå·±"),
-    FAILED_USER_ALREADY_EXIST("486", "è¯¥ç”¨æˆ·å·²å­˜åœ¨"),
-    INTERNAL_SERVER_ERROR("500", "æœåŠ¡å™¨å†…éƒ¨é”™è¯¯"),
-    NOT_IMPLEMENTED("501", "ä¸šåŠ¡å¼‚å¸¸"),
-    CUOWO("502", "æ—¶é—´åç§°æ˜¯å”¯ä¸€çš„");
->>>>>>> 8fb5d7915b12b450a648d8653773f9d4aa5f38fd
+    FAILED_CLASSIFYING_ALREADY_EXIST("446","ÊÇ·ñ·Ö¼¶Ñ¡Ôñ¡°ÊÇ¡±ºó,²»ÄÜÐÞ¸ÄÎª¡°·ñ¡±£¡£¡"),
+    CUOWO("502", "Ê±¼äÃû³ÆÊÇÎ¨Ò»µÄ");
+
 
     private String code;
     private String msg;
